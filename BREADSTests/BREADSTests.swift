6fd2c392fcf26f3978a9b2deeb0c628fd32b4479
DharaniReddy <dharani.reddy@emids.com>
2018-12-10 19:02:08 +0530

    Initial Commit

---
+++ BREADSTests/BREADSTests.swift
@@ -0,0 +1,34 @@
+//
+//  BREADSTests.swift
+//  BREADSTests
+//
+//  Created by Dharani Reddy on 10/12/18.
+//  Copyright © 2018 Integro Infotech. All rights reserved.
+//
+
+import XCTest
+@testable import BREADS
+
+class BREADSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,20 @@
+platform :ios, '10.0'
+inhibit_all_warnings!
+
+def list_of_library
+    use_frameworks!
+    # more pods...
+    pod 'SlideMenuControllerSwift'
+end
+
+target 'BREADS' do
+  list_of_library
+end
+
+target 'BREADSTests' do
+    list_of_library
+end
+
+target 'BREADSUITests' do
+    list_of_library
+end
